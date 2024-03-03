d875d095de545ca671327f4c1cecbe6063b2df63
Fenson-F <fdfenlon@gmail.com>
2024-03-03 17:21:22 -0500

    Fixed SSH agent

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'ef8af20b144da1967946e80b922302783c0b0771'
+AESD_ASSIGNMENTS_VERSION = '9bdea226dea5cf66b4e99b3d955039a25ecda060'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
